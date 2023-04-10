e3f3702042ca36527f6c336097d2b28fb5e5b315
mindkeeper <fcb.nyak@gmail.com>
2023-04-10 15:24:57 +0700

    updating directory

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 const ejs = require("ejs");
 const { google } = require("googleapis");
-const rootDir = require("../helpers/rootDir");
+const rootDir = require("./rootDir");
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
@@ -32,7 +32,6 @@ const sendEmail = (userData, subject) =>
         accessToken,
       },
     });
-
     const p = path.join(rootDir, "src", "views", "forgot-password.ejs");
     ejs.renderFile(p, { data: userData, subject }, (err, data) => {
       if (err) {
