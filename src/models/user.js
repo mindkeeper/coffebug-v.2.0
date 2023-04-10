5554efc33841d312cf7222ee4c41b690f0e14c22
mindkeeper <fcb.nyak@gmail.com>
2023-04-10 19:49:26 +0700

    add verified and register token column

--- src/models/user.js
+++ src/models/user.js
@@ -14,6 +14,12 @@ const User = sequelize.define(
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
+    verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    registerToken: { type: DataTypes.STRING, allowNull: true },
   },
   {
     paranoid: true,
