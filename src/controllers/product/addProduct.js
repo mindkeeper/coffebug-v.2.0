c139953acfe224b6274d9fc8a277ee8e61c538c9
mindkeeper <fcb.nyak@gmail.com>
2023-05-07 15:10:41 +0700

    implementing add product

--- src/controllers/product/addProduct.js
+++ src/controllers/product/addProduct.js
@@ -1,10 +1,27 @@
+const sequelize = require("../../config/sequelize");
+const Product = require("../../models/product");
+const ProductImages = require("../../models/productImages");
 const addProduct = async (req, res, next) => {
+  const t = await sequelize.transaction();
   try {
     const uploadedFiles = req.uploadedFiles;
-    const { body, files } = req;
-    return res.sendSuccess(200, { body, files });
+    const { title, description, price, quantity, categoryId } = req.body;
+    const product = await Product.create(
+      { title, description, quantity, price, categoryId },
+      { transaction: t }
+    );
+    const imageUpload = await ProductImages.bulkCreate(
+      uploadedFiles.map((value) => ({
+        imageUrl: value,
+        productId: product.id,
+      })),
+      { transaction: t }
+    );
+    await t.commit();
+    res.sendSuccess(201, { product, imageUpload });
   } catch (error) {
     console.log(error);
+    t.rollback();
     res.sendServerError();
   }
 };
