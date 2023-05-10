13cfe8ae295b6319d43170e45e1408c345b8fe82
mindkeeper <fcb.nyak@gmail.com>
2023-05-10 15:41:58 +0700

    add pagination helper

---
+++ src/helpers/paginate.js
@@ -0,0 +1,17 @@
+const paginate = (totalData, limit, currentPage) => {
+  const totalPage = Math.ceil(totalData / 2 / limit);
+  const hasNextPage = currentPage < totalPage;
+  const hasPrevPage = currentPage > 1;
+
+  const paginatiion = {
+    currentPage: parseInt(currentPage),
+    totalPage,
+    totalData,
+    pageSze: parseInt(limit),
+    prev: hasPrevPage ? currentPage - 1 : null,
+    next: hasNextPage ? currentPage + 1 : null,
+  };
+  return paginatiion;
+};
+
+module.exports = paginate;
